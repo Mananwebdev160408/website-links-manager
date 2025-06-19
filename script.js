fb237b2076def645f168609183b2d968b389ff41
Manan Gupta <guptamanan576@gmail.com>
2025-06-19 19:18:26 +0530

    that non text click wala problem done

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ let ul=document.getElementsByTagName('ul')[0]
 btn.addEventListener('click',()=>{
     let name=prompt('name of website')
     let url=prompt('link of the website')
-    let li = ul.insertAdjacentHTML("afterbegin",`<li><a href="${url}">${name}</a></li>`)
+    let li = ul.insertAdjacentHTML("afterbegin",`<a href="${url}"><li>${name}</li></a>`)
     localStorage.setItem(name,url)
 })
 let a =localStorage.length
 for(i=0;i<a;i++){
     let out=localStorage.key(i)
     let page=localStorage.getItem(out)
-    let li = ul.insertAdjacentHTML("afterbegin", `<li><a href='${page}'>${out}</a></li>`)
+    let li = ul.insertAdjacentHTML("afterbegin", `<a href='${page}'><li>${out}</li></a>`)
 }
